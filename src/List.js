8724c63c99b4b1844f568839d88041925d4189bd
Mariano Estrada <mariano-e-s@hotmail.com>
2021-09-28 14:14:49 -0500

    birthday app

---
+++ src/List.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const List = ({people}) => { //people viene de mi data
+  return (
+    <>
+      {people.map((person)=>{
+        const{id, name, age, image}=person //Desestructuring de las propiedades de cada person
+        return(
+          <article key={id} className="person">
+            <img src={image} alt={name}/>
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        )
+      })}
+      
+    </>
+  );
+};
+
+export default List;
